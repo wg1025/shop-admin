d2cfe8b7149699957cba9b7d702126547f9ad5e7
wugang <wugang@168.com>
2019-06-27 22:56:05 +0800

    1.项目接本架构完成 2.登录功能实现一半 3.开始招聘程序员

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+//  1. 引入ElemnetUI库
+import ElementUI from "element-ui";
+// 2.引入css文件
+import "element-ui/lib/theme-chalk/index.css";
+
+import "./assets/css/index.css";
+// 3. 安装ElementUI
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
---
+++ src/views/login.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- el-row 一行  用来布局-->
+  <el-row type="flex" class="row-bg" justify="center" align="middle">
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
+      <el-form ref="form" :model="form" class="login-form" label-position="top" label-width="80px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.usename"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">登录</el-button>
+          <el-button>重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        usename: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    }
+  }
+};
+</script>
+<style>
+.row-bg {
+  height: 100%;
+  background-color: #2d434c;
+}
+.login-form {
+  background: #fff;
+  padding: 30px 20px;
+  border-radius: 10px;
+  min-width: 400px;
+}
+</style>
